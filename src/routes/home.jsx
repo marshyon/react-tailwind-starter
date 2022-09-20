a274576eb89f9491900e929272bf37748acfca96
jon brookes <marshyon@gmail.com>
2022-09-20 15:12:43 +0100

    initial commit

---
+++ src/routes/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+export default function home() {
+    return (
+        <>
+            <div className="container mx-auto bg-blue-50 rounded-xl shadow border p-8 m-10">
+                <p className="text-3xl text-primary font-bold mb-5">
+                    HOME
+                </p>
+                <p className="text-secondary text-lg">
+                    React React Router Dom V6 and Tailwind CSS in action
+                </p>
+            </div>
+        </>
+    )
+}
